8c956bc5e79d39bd7cb8dde98b57ec8875335e9b
ta88cake <takekazu8888@gmail.com>
2025-08-01 14:27:51 +0900

    style: Format backend.tf

--- backend.tf
+++ backend.tf
@@ -4,10 +4,10 @@ terraform {
   backend "s3" {
     # ↓↓↓ ステップ1で作ったS3バケットの名前に書き換える
     bucket = "tfstate-your-unique-name-20240730"
-    
+
     # S3バケットの中に、この名前でtfstateファイルが作られる
-    key    = "terraform.tfstate"
-    
+    key = "terraform.tfstate"
+
     # S3バケットが存在するリージョン
     region = "ap-northeast-1"
   }
